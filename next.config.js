f7caa1b33066197f9bc885bb806755a2992af4cf
William Li <email@williamli.dev>
2023-07-19 15:18:59 +0800

    set CSP frame-ancestors *

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';
+  frame-ancestors *;
+`;
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // this gets converted to /(en|fr|de)/(.*) so will not match the top-level
+        // `/` or `/fr` routes like /:path* would
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+          }
+        ]
+      }
+    ];
+  }
+};
+
+module.exports = nextConfig;
